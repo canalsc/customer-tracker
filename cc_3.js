9b15081dcfd799ded4395d9b73c8b7b9678f93dd
Carmen Canals <canalsc@usf.edu>
2026-02-02 16:05:30 -0500

    changed syntax for the cc_3.js file and added a title to the html file

--- cc_3.js
+++ cc_3.js
@@ -1,18 +1,28 @@
 // Customer Records
-let customers = { 
-    Kylo_Ren: { 
+let customers = [
+    { 
         name: "Kylo Ren", 
         email: "Kylo.ren@gmail.com",  
         purchases: ["lightsaber", "mask", "hair dye"]
     }, 
-    Anakin_skywalker: { 
+    { 
         name: "Anakin Skywalker", 
         email: "chosen.1@gmail.com", 
         purchases: ["scar cream", "droid parts", "black gloves"] 
     }, 
-    Yoda: { 
+    { 
         name: "Yoda", 
         email: "teachyou.ican@gmail.com", 
         purchases: ["cane", "spices", "training saber"] 
     }
-};
+]; 
+
+//Adding and Removing Data 
+customers.push(
+    { 
+        name: "Obi-Wan Kenobi", 
+        email: "kenobi.jedi@gmail.com", 
+        purchases: ["beard conditioner", "sunscreen", "light robes"]
+    } 
+) 
+customers.shift("kylo Ren")
