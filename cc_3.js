8dd5669b3cbe68dcfb15d220a5ec30128e7b63d0
Carmen Canals <canalsc@usf.edu>
2026-02-02 15:41:10 -0500

    created customer records for three customers

---
+++ cc_3.js
@@ -0,0 +1,18 @@
+// Customer Records
+let customers = { 
+    properties01: { 
+        name: "Kylo Ren", 
+        email: "Kylo.ren@gmail.com",  
+        purchases: ["lightsaber", "mask", "hair dye"]
+    }, 
+    properties02: { 
+        name: "Anakin Skywalker", 
+        email: "chosen.1@gmail.com", 
+        purchases: ["scar cream", "droid parts", "black gloves"] 
+    }, 
+    properties03: { 
+        name: "Yoda", 
+        email: "teachyou.ican@gmail.com", 
+        purchases: ["cane", "spices", "training saber"] 
+    }
+};
